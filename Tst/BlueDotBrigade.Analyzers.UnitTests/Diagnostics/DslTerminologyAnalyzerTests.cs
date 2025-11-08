765d6acbe83333ec79a641559e41946848fb1bd1
Pressacco <5507864+Pressacco@users.noreply.github.com>
2025-11-08 17:48:23 -0500

    Add DSL terminology pass/fail coverage for identifier kinds

--- Tst/BlueDotBrigade.Analyzers.UnitTests/Diagnostics/DslTerminologyAnalyzerTests.cs
+++ Tst/BlueDotBrigade.Analyzers.UnitTests/Diagnostics/DslTerminologyAnalyzerTests.cs
@@ -7,6 +7,13 @@ namespace BlueDotBrigade.Analyzers.Diagnostics
     [TestClass]
     public class DslTerminologyAnalyzerTests
     {
+        private const string DslPreferCustomerBlockCust = """
+<?xml version="1.0" encoding="utf-8"?>
+<dsl>
+  <term prefer="Customer" block="Cust" case="sensitive" />
+</dsl>
+""";
+
         [TestMethod]
         public async Task NoDiagnostics_When_NoBlockedTerms()
         {
@@ -93,6 +100,221 @@ namespace BlueDotBrigade.Analyzers.Diagnostics
                 CSharpAnalyzerVerifier.Diagnostic("BDB000"));
 
             await test.RunAsync();
+        }
+
+        [TestMethod]
+        public async Task ClassName_Pass_When_UsingPreferredTerm()
+        {
+            var test = CreateTest("""
+namespace Sample;
+
+public class Customer
+{
+}
+""");
+
+            await test.RunAsync();
+        }
+
+        [TestMethod]
+        public async Task FieldName_Pass_When_UsingPreferredTerm()
+        {
+            var test = CreateTest("""
+namespace Sample;
+
+public class Example
+{
+    private int CustomerCount;
+}
+""");
+
+            await test.RunAsync();
+        }
+
+        [TestMethod]
+        public async Task PropertyName_Pass_When_UsingPreferredTerm()
+        {
+            var test = CreateTest("""
+namespace Sample;
+
+public class Example
+{
+    public string PreferredCustomer { get; set; }
+}
+""");
+
+            await test.RunAsync();
+        }
+
+        [TestMethod]
+        public async Task MethodName_Pass_When_UsingPreferredTerm()
+        {
+            var test = CreateTest("""
+namespace Sample;
+
+public class Example
+{
+    public void CustomerInfluencer() { }
+}
+""");
+
+            await test.RunAsync();
+        }
+
+        [TestMethod]
+        public async Task ParameterName_Pass_When_UsingPreferredTerm()
+        {
+            var test = CreateTest("""
+namespace Sample;
+
+public class Example
+{
+    public void Process(string PreferredCustomer) { }
+}
+""");
+
+            await test.RunAsync();
+        }
+
+        [TestMethod]
+        public async Task LocalVariableName_Pass_When_UsingPreferredTerm()
+        {
+            var test = CreateTest("""
+namespace Sample;
+
+public class Example
+{
+    public void Process()
+    {
+        var customerValue = 0;
+    }
+}
+""");
+
+            await test.RunAsync();
+        }
+
+        [TestMethod]
+        public async Task ClassName_Fail_When_UsingBlockedTerm()
+        {
+            var test = CreateTest("""
+namespace Sample;
+
+public class Cust
+{
+}
+""");
+
+            test.ExpectedDiagnostics.Add(
+                CSharpAnalyzerVerifier.Diagnostic("BDB001").WithSpan(3,14,3,18));
+
+            await test.RunAsync();
+        }
+
+        [TestMethod]
+        public async Task FieldName_Fail_When_UsingBlockedTerm()
+        {
+            var test = CreateTest("""
+namespace Sample;
+
+public class Example
+{
+    private int CustCount;
+}
+""");
+
+            test.ExpectedDiagnostics.Add(
+                CSharpAnalyzerVerifier.Diagnostic("BDB001").WithSpan(5,17,5,26));
+
+            await test.RunAsync();
+        }
+
+        [TestMethod]
+        public async Task PropertyName_Fail_When_UsingBlockedTerm()
+        {
+            var test = CreateTest("""
+namespace Sample;
+
+public class Example
+{
+    public string PreferredCust { get; set; }
+}
+""");
+
+            test.ExpectedDiagnostics.Add(
+                CSharpAnalyzerVerifier.Diagnostic("BDB001").WithSpan(5,19,5,32));
+
+            await test.RunAsync();
+        }
+
+        [TestMethod]
+        public async Task MethodName_Fail_When_UsingBlockedTerm()
+        {
+            var test = CreateTest("""
+namespace Sample;
+
+public class Example
+{
+    public void CustInfluencer() { }
+}
+""");
+
+            test.ExpectedDiagnostics.Add(
+                CSharpAnalyzerVerifier.Diagnostic("BDB001").WithSpan(5,17,5,31));
+
+            await test.RunAsync();
+        }
+
+        [TestMethod]
+        public async Task ParameterName_Fail_When_UsingBlockedTerm()
+        {
+            var test = CreateTest("""
+namespace Sample;
+
+public class Example
+{
+    public void Process(string PreferredCust) { }
+}
+""");
+
+            test.ExpectedDiagnostics.Add(
+                CSharpAnalyzerVerifier.Diagnostic("BDB001").WithSpan(5,32,5,45));
+
+            await test.RunAsync();
+        }
+
+        [TestMethod]
+        public async Task LocalVariableName_Fail_When_UsingBlockedTerm()
+        {
+            var test = CreateTest("""
+namespace Sample;
+
+public class Example
+{
+    public void Process()
+    {
+        var CustValue = 0;
+    }
+}
+""");
+
+            test.ExpectedDiagnostics.Add(
+                CSharpAnalyzerVerifier.Diagnostic("BDB001").WithSpan(7,13,7,22));
+
+            await test.RunAsync();
+        }
+
+        private static CSharpAnalyzerVerifier.Test CreateTest(string code)
+        {
+            var test = new CSharpAnalyzerVerifier.Test
+            {
+                TestCode = code,
+            };
+
+            test.TestState.AdditionalFiles.Add(("src/TestProj/dsl.config.xml", DslPreferCustomerBlockCust));
+            test.TestState.AdditionalFiles.Add(("/.editorconfig", "build_property.MSBuildProjectDirectory = src/TestProj"));
+
+            return test;
         }
     }
 }
